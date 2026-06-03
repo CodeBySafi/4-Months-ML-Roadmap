8ee367f3e872f14b4c9b4dff58d338de36e1c5dc
Safiullah <safiullahch87@gmail.com>
2026-06-03 17:10:06 +0500

    Day 1: Added temperature converter file

---
+++ day1.py
@@ -0,0 +1,44 @@
+
+
+
+def celcius(f):
+    return "Temperature in celcius is :", (f-32)*5/9
+
+def farenheit(c):
+    return "Temperature in farenheit is ",(c*9/5)+32
+
+menu=(" ___________WELCOME TO TEMPERATURE CONVERTER __________________ \n"
+"1.CELCIUS \n "
+"2.FARENHEIT \n"
+"0.EXIT \n")
+
+while True:
+    try:
+        num=int(input(menu))
+        if num==1:
+            tem=float(input("Enter temperature in farenheit :"))
+            print(celcius(tem))
+
+        elif num==2:
+            tem=float(input("Enter temperature in Celcius"))
+            print(farenheit(tem))
+        
+        elif num==0:
+            print("Thankyou")
+            break
+        else:
+            print("Inavlid choice")
+    
+    except:  
+        print("Please Enter number")
+
+
+
+
+
+
+
+
+    
+
+
