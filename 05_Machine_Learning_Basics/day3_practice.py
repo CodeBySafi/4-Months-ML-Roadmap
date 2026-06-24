c0ccdaae217e3ed222a51389276cc3b19ff73fe1
Safiullah <safiullahch87@gmail.com>
2026-06-24 14:27:03 +0500

    Add Day 3 Task: Outlier treatment using Capping (Winsorization) method

---
+++ 05_Machine_Learning_Basics/day3_practice.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# --- Dummy Data: Server Traffic ---
+data_server = {
+    'Response_Time_ms': [12, 15, 20, 18, -5, 22, 25, 30, 8000, 28, 35, 40, 45, 50, 48, 55, 60, 65, 70, 75],
+    'Traffic_Status': ['Low', 'Low', 'Low', 'Low', 'Low', 'Low', 'Low', 'Low', 'High', 'Low', 
+                       'High', 'High', 'High', 'High', 'High', 'High', 'High', 'High', 'High', 'High']
+}
+df_server = pd.DataFrame(data_server)
+print("--- Original Data ---")
+print(df_server.head())
+
+# ==========================================
+# 👇 YAHAN SE NEECHAY AAPNE CODE LIKHNA HAI 👇
+# ==========================================
+
+# Step 1: X aur y banayein ('Response_Time_ms' X hoga, aur 'Traffic_Status' y hoga)
+# NOTE: X ko 2D rakhne ke liye double brackets use kijiye ga: df_server[['Response_Time_ms']]
+
+
+# Step 2: train_test_split karein (test_size=0.25, random_state=42, stratify=y zaroor lagayein)
+
+
+# Step 3: Golden Rule! Sirf X_train ke 'Response_Time_ms' se lower_limit (0.05) aur upper_limit (0.95) nikalein
+
+
+# Step 4: X_train par np.where() laga kar Capping karein
+
+
+# Step 5: X_test par BHI np.where() laga kar Capping karein (Using Train limits)
+
+
+#
+
+X=df_server[['Response_Time_ms']]
+y=df_server[ 'Traffic_Status']
+
+X_train,X_test,y_train,y_test=train_test_split(
+X,
+y,
+test_size=0.25, random_state=42, stratify=y 
+
+)
+
+upper=X_train['Response_Time_ms'].quantile(0.95)
+lower=X_train['Response_Time_ms'].quantile(0.05)
+
+trim_X_train=X_train.loc[(X_train['Response_Time_ms']>=lower)&(X_train['Response_Time_ms']<=upper)]
+trim_X_test=X_test.loc[(X_test['Response_Time_ms']>=lower)&(X_test['Response_Time_ms']<=upper)]
+
+
+print(trim_X_test)
+print(trim_X_train)
+
+
+cap_X_train=np.where(X_train['Response_Time_ms']>upper,upper,np.where(X_train['Response_Time_ms']<lower,lower,X_train['Response_Time_ms']))
+cap_X_test=np.where(X_test['Response_Time_ms']>upper,upper,np.where(X_test['Response_Time_ms']<lower,lower,X_test['Response_Time_ms']))
+
+
+
+print("\n--- Capped X_train (Dekhein -5 aur 8000 theek hue?) ---")
+print(cap_X_train)
+
+print("\n--- Capped X_test ---")
+print(cap_X_test)
